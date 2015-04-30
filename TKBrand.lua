2480e11026929c375f8247868c387f0d010cbee3
nebelwolfi <dr.backdooring@gmail.com>
2015-04-30 07:03:09 +0200

    asdf

--- TKBrand.lua
+++ TKBrand.lua
@@ -1,15 +1,44 @@
 --[[
 
-  ____                      _ 
- |  _ \                    | |
- | |_) |_ __ __ _ _ __   __| |
- |  _ <| '__/ _` | '_ \ / _` |
- | |_) | | | (_| | | | | (_| |
- |____/|_|  \__,_|_| |_|\__,_|
-                              
+  _______            _  __    _      ____                      _ 
+ |__   __|          | |/ /   | |    |  _ \                    | |
+    | | ___  _ __   | ' / ___| | __ | |_) |_ __ __ _ _ __   __| |
+    | |/ _ \| '_ \  |  < / _ \ |/ / |  _ <| '__/ _` | '_ \ / _` |
+    | | (_) | |_) | | . \  __/   <  | |_) | | | (_| | | | | (_| |
+    |_|\___/| .__/  |_|\_\___|_|\_\ |____/|_|  \__,_|_| |_|\__,_|
+            | |                                                  
+            |_|                                                  
+
   By Nebelwolfi                             
  
 ]]--
+
+--[[ Auto updater start ]]--
+local version = 0.01
+local AUTO_UPDATE = true
+local UPDATE_HOST = "raw.github.com"
+local UPDATE_PATH = "/nebelwolfi/BoL/master/TKBrand.lua".."?rand="..math.random(1,10000)
+local UPDATE_FILE_PATH = SCRIPT_PATH.."TKBrand.lua"
+local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
+local function AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>[Top Kek Series]: Brand - </b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
+if AUTO_UPDATE then
+  local ServerData = GetWebResult(UPDATE_HOST, "/nebelwolfi/BoL/master/TKBrand.version")
+  if ServerData then
+    ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
+    if ServerVersion then
+      if tonumber(version) < ServerVersion then
+        AutoupdaterMsg("New version available v"..ServerVersion)
+        AutoupdaterMsg("Updating, please don't press F9")
+        DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)
+      else
+        AutoupdaterMsg("Loaded the latest version (v"..ServerVersion..")")
+      end
+    end
+  else
+    AutoupdaterMsg("Error downloading version info")
+  end
+end
+--[[ Auto updater end ]]--
 
 --[[ Assassin list start ]]--
 _G.Champs = {
