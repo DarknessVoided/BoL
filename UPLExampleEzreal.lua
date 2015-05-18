68fc0d1a4ecdf28620f361ac64696d4b311da100
nebelwolfi <dr.backdooring@gmail.com>
2015-05-18 17:03:16 +0200

    asdf

--- UPLExampleEzreal.lua
+++ UPLExampleEzreal.lua
@@ -1,19 +1,20 @@
 require("UPL")
 UPL = UPL()
+
 function OnLoad()
     Config = scriptConfig("Ezreal Q Aim", "UPLExampleEzreal")
     Config:addParam("shoot", "Shoot", SCRIPT_PARAM_ONKEYDOWN, false, 32)
     Config:permaShow("shoot")
-    sts = TargetSelector(TARGET_LESS_CAST, 1500, DAMAGE_MAGIC, true)
-
+    targetSelector = TargetSelector(TARGET_LESS_CAST, 1500, DAMAGE_MAGIC, true)
     Config:addParam("hc", "Accuracy (Default: 2)", SCRIPT_PARAM_SLICE, 2, 0, 3, 1)
     UPL:AddToMenu(Config)
     UPL:AddSpell(_Q, { speed = 1975, delay = 0.25, range = 1200, width = 50, collision = true, aoe = false, type = "linear" })
 end
+
 function OnTick()
-    sts:update()
-    if sts.target == nil or not Config.shoot then return end
-    CastPosition, HitChance, HeroPosition = UPL:Predict(_Q, myHero, sts.target)
+    targetSelector:update()
+    if targetSelector.target == nil or not Config.shoot then return end
+    CastPosition, HitChance, HeroPosition = UPL:Predict(_Q, myHero, targetSelector.target)
     if HitChance >= Config.hc then
       CastSpell(_Q, CastPosition.x, CastPosition.z)
     end
