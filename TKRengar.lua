ca44084efdab7236000d9a497960a16a972bb07b
nebelwolfi <dr.backdooring@gmail.com>
2015-05-19 00:48:50 +0200

    asdf

--- TKRengar.lua
+++ TKRengar.lua
@@ -14,7 +14,7 @@
 ]]--
 
 --[[ Auto updater start ]]--
-local version = 0.15
+local version = 0.16
 local AUTO_UPDATE = true
 local UPDATE_HOST = "raw.github.com"
 local UPDATE_PATH = "/nebelwolfi/BoL/master/TKRengar.lua".."?rand="..math.random(1,10000)
@@ -97,7 +97,7 @@ local enemyCount = 0
 local ultOn, oneShot = false, false
 local osTarget = nil
 data = {
-    [_Q] = { range = myHer.range, type = "notarget", aareset = true},
+    [_Q] = { range = myHero.range, type = "notarget", aareset = true},
     [_W] = { speed = math.huge, delay = 0.5, range = 390, width = 55, collision = false, aoe = true, type = "circular"},
     [_E] = { speed = 1500, delay = 0.50, range = 1000, width = 80, collision = false, aoe = false, type = "linear"},
     [_R] = { range = 4000, type = "notarget"}
