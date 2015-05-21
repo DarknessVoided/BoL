04d984f9d991097dbf20515d6e58679a3142a4b2
nebelwolfi <dr.backdooring@gmail.com>
2015-05-21 11:08:59 +0200

    asdf

--- Common/UPL.lua
+++ Common/UPL.lua
@@ -189,7 +189,7 @@ function UPL:DPredict(Target, spell, source)
   elseif spell.type == "cone" then
     Spell = ConeSS(spell.speed, spell.range, spell.width, spell.delay * 1000, col)
   end
-  return self.DP:predict(unit, Spell, 1.2, Vector(source))--, source)
+  return self.DP:predict(unit, Spell, 1.2, Vector(source))
 end
 
 function UPL:VPredict(Target, spell, source)
