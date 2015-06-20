2be57af204d732f797c6c8cb5c2dafee1733964f
nebelwolfi <dr.backdooring@gmail.com>
2015-06-20 22:13:37 +0200

    asdf

--- testers_and_getters/0SPredTester.lua
+++ testers_and_getters/0SPredTester.lua
@@ -8,28 +8,29 @@ local pos, chance, hpos, hhpos, hposh, hchance
 
 function OnLoad() 
   	Config = scriptConfig("Scriptology Prediction Tester", "SPT")
-  	Config:addParam("s", "Shoot", SCRIPT_PARAM_ONKEYDOWN, false, 32)
-  	Config:addParam("hc","Hitchance", SCRIPT_PARAM_SLICE, 0, 0, 4, 1)
+  	Config:addParam("s", "Shoot", SCRIPT_PARAM_ONKEYTOGGLE, false, 32)
+  	Config:addParam("hc","Hitchance", SCRIPT_PARAM_SLICE, 2, 0, 3, 0)
   	Config:addParam("spell","Spell", SCRIPT_PARAM_SLICE, 0, 0, 3, 0)
   	Config:addParam("p", "Print", SCRIPT_PARAM_ONOFF, false)
-  	sts = TargetSelector(TARGET_LESS_CAST, 1200, DAMAGE_MAGIC)
+  	Config:addParam("d", "Draw", SCRIPT_PARAM_ONOFF, false)
+  	sts = TargetSelector(TARGET_LESS_CAST, 3500, DAMAGE_MAGIC)
 end
 
 function OnTick()
 	spell = Config.spell
+	local speed, delay, range, width, coll, type = SP:GetData(spell, myHero)
 	sts:update()
 	Target = sts.target
 	for _,v in pairs(GetEnemyHeroes()) do
-		if v.visible and (not v.dead) then 
-			if GetDistance(myHero,v) <= 1175 and GetDistance(myHero,v) >= minHitDistance and (not Target or v.health < Target.health) then Target = v end
+		if not v.dead then 
+			if range and width and GetDistance(myHero,v) <= range+width and GetDistance(myHero,v) >= minHitDistance and (not Target or v.health < Target.health) then Target = v end
 		end
 	end
 	if Config.s then
-		--myHero:MoveTo(mousePos.x,mousePos.z)
-		if Target ~= nil and GetDistance(Target, myHero) < 1175 then
+		if Target ~= nil then
 			hposh, hchance, hhpos = SP:Predict(spell, myHero, Target)
 			if hchance and hchance >= Config.hc then
-				print("Shootchance:"..hchance)
+				if Config.p then print("Shootchance:"..hchance) end
 				CastSpell(spell, hposh.x, hposh.z)
 			end
 		end
@@ -40,24 +41,20 @@ function OnTick()
 end
 
 function OnDraw()
+	if not Config.d then return end
 	local speed, delay, range, width, coll, type = SP:GetData(spell, myHero)
 	DrawCircle3D(myHero.x, myHero.y, myHero.z, range, 1, ARGB(255,255,255,255), 32)
 
 	DrawHitBox(myHero, 2, ARGB(255,255,255,255))
-	--DrawCircle3D(myHero.x, myHero.y, myHero.z, GetDistance(myHero.minBBox, myHero.pos), 1, ARGB(255,255,255,255), 32)
 	ppos, range = SP:PredictPos(myHero)
 	DrawCircle3D(ppos.x, ppos.y, ppos.z, range, 1, ARGB(255,255,255,255), 32)
     DrawLine3D(myHero.x, myHero.y, myHero.z, ppos.x, ppos.y, ppos.z, 3, ARGB(255, 55, 55, 55))
 
     if myHero.hasMovePath and myHero.pathCount >= 2 then
       local IndexPath = myHero:GetPath(myHero.pathIndex)
-      if IndexPath then
-        --DrawLine3D(myHero.x, myHero.y, myHero.z, IndexPath.x, IndexPath.y, IndexPath.z, 1, ARGB(255, 255, 255, 255))
-      end
       for i=myHero.pathIndex, myHero.pathCount-1 do
         local Path = myHero:GetPath(i)
         local Path2 = myHero:GetPath(i+1)
-        --DrawLine3D(Path.x, Path.y, Path.z, Path2.x, Path2.y, Path2.z, 1, ARGB(255, 255, 255, 255))
       end
     end
     for i, enemy in ipairs(GetEnemyHeroes()) do
@@ -79,7 +76,7 @@ function OnDraw()
 	if predTarget ~= nil then
 		if not Config.s then
 			pos, chance, hpos = SP:Predict(spell, myHero, predTarget)
-			if chance and chance > 0 then print(predTarget.charName.." Hitchance:"..chance) end
+			if chance and chance > 0 and Config.p then print(predTarget.charName.." Hitchance:"..chance) end
 		end
 		ppos, range = SP:PredictPos(predTarget)
 		DrawCircle3D(ppos.x, ppos.y, ppos.z, range, 1, ARGB(255,255,255,255), 32)
