86e3bfb1d66aa544fc9ccca481bf3978c3890def
nebelwolfi <dr.backdooring@gmail.com>
2015-07-24 20:47:56 +0200

    asdf

--- SPlugins/Gangplank.lua
+++ SPlugins/Gangplank.lua
@@ -4,9 +4,9 @@ class "Gangplank"
   function Gangplank:__init()
     targetSel = TargetSelector(TARGET_LESS_CAST_PRIORITY, 1150, DAMAGE_PHYSICAL, false, true)
     data = {
-      [_Q] = { speed = 1200, delay = 0.5, range = 1150, width = 40, collision = true, aoe = false, type = "linear", dmgAD = function(AP, level, Level, TotalDmg, source, target) return 0-50+60*level+TotalDmg end},
+      [_Q] = { speed = 1200, delay = 0.5, range = 1150, width = 40, collision = true, aoe = false, type = "linear"},
       [_W] = { delay = 1.5, range = 5000},
-      [_E] = { range = 1000, dmgAD = function(AP, level, Level, TotalDmg, source, target) return GetStacks(target) > 0 and (10 + (10 * level) + (TotalDmg * 0.6)) + (GetStacks(target)-1) * (kalE(level) + (0.175 + 0.025 * level)*TotalDmg) or 0 end},
+      [_E] = { range = 1000},
       [_R] = { range = 2000}
     }
   end
@@ -44,4 +44,16 @@ class "Gangplank"
     Config.kConfig:addDynamicParam("Harrass", "Harrass", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("C"))
     Config.kConfig:addDynamicParam("LastHit", "Last hit", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("X"))
     Config.kConfig:addDynamicParam("LaneClear", "Lane Clear", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("V"))
+  end
+
+  function Gangplank:Tick()
+	  for i = 1, myHero.buffCount, 1 do      
+		  local buff = myHero:getBuff(i)
+		  if buff.name and buff.valid then print(buff.type.." "..buff.name)  end
+		  if buff.valid and buff.name and (buff.type == 24 or buff.type == 5 or buff.type == 11 or buff.type == 22 or buff.type == 21 or buff.type == 8)
+		  or (buff.type == 10 and buff.name and buff.name:lower():find("fleeslow"))
+		  or buff.name and buff.name:lower():find("summonerexhaust")) then
+			  Cast(_W)
+		  end                    
+	  end  
   end
