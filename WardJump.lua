a74eae39bc31a18a61c4a623ef69d524d9486607
nebelwolfi <dr.backdooring@gmail.com>
2015-08-04 18:58:29 +0200

    asdf

--- WardJump.lua
+++ WardJump.lua
@@ -9,7 +9,7 @@ function OnLoad()
 	casted, jumped = false, false
 	for i = 1, objManager.maxObjects do
 		local object = objManager:GetObject(i)
-		if object ~= nil and object.valid and (string.find(string.lower(object.name), "ward") or string.find(string.lower(myHero:GetSpellData(slot).name), "trinkettotem")) then
+		if object ~= nil and object.valid and (object.name:lower():find("ward") or object.name:lower():find("trinkettotem")) then
 			table.insert(Wards, object)
 		end
 	end
@@ -23,24 +23,26 @@ end
 
 function OnDraw()
 	if Config.d then
-		local pos = getMousePos()
+		local pos = Vector(myHero) - (Vector(myHero) - mousePos):normalized() * (myHero.charName == "LeeSin" and 600 or 700)
 		DrawCircle3D(pos.x,pos.y,pos.z,150,1,ARGB(255,255,255,255),32)
 	end
 end
 
 function WardJump()
-	if casted and jumped then casted, jumped = false, false
-	elseif (myHero:CanUseSpell(_W) == READY and myHero.charName == "LeeSin" and myHero:GetSpellData(_W).name == "BlindMonkWOne") or (myHero:CanUseSpell(_E) == READY and myHero.charName == "Katarina") or (myHero:CanUseSpell(_Q) == READY and myHero.charName == "Jax") then
-		local pos = getMousePos()
-		if Jump(pos, 250, true) then return end
+	if casted and jumped then 
+		casted, jumped = false, false
+	elseif (myHero:CanUseSpell(toCast[myHero.charName]) == READY and myHero:GetSpellData(_W).name == "BlindMonkWOne") 
+		or (myHero:CanUseSpell(toCast[myHero.charName]) == READY and myHero.charName ~= "LeeSin") then
+		local pos = Vector(myHero) - (Vector(myHero) - mousePos):normalized() * (myHero.charName == "LeeSin" and 600 or 700)
+		if Jump(pos, 250) then return end
 		slot = GetWardSlot()
-		if not slot then return end
+		if not slot or casted then return end
 		CastSpell(slot, pos.x, pos.z)
 		casted = true
 	end
 end
 
-function Jump(pos, range, useWard)
+function Jump(pos, range)
 	for _,ally in pairs(GetAllyHeroes()) do
 		if (GetDistance(ally, pos) <= range) then
 			CastSpell(toCast[myHero.charName], ally)
@@ -67,31 +69,25 @@ end
 
 function OnCreateObj(obj)
 	if obj ~= nil and obj.valid then
-		if string.find(string.lower(obj.name), "ward") or string.find(string.lower(myHero:GetSpellData(slot).name), "trinkettotem") then
+		if obj.name:lower():find("ward") or obj.name:lower():find("trinkettotem") then
 			table.insert(Wards, obj)
 		end
 	end
 end
 
-function getMousePos(range)
-	local MyPos = Vector(myHero.x, myHero.y, myHero.z)
-	local MousePos = Vector(mousePos.x, mousePos.y, mousePos.z)
-	return MyPos - (MyPos - MousePos):normalized() * (myHero.charName == "LeeSin" and 600 or 700)
-end
-
 function GetWardSlot()
 	for slot = ITEM_7, ITEM_1, -1 do
-		if myHero:GetSpellData(slot).name and myHero:CanUseSpell(slot) == READY and string.find(string.lower(myHero:GetSpellData(slot).name), "trinkettotem") then
+		if myHero:GetSpellData(slot).name and myHero:CanUseSpell(slot) == READY and myHero:GetSpellData(slot).name:lower():find("trinkettotem") then
 			return slot
 		end
 	end
 	for slot = ITEM_7, ITEM_1, -1 do
-		if myHero:GetSpellData(slot).name and myHero:CanUseSpell(slot) == READY and (string.find(string.lower(myHero:GetSpellData(slot).name), "ward") and not string.find(string.lower(myHero:GetSpellData(slot).name), "vision")) then
+		if myHero:GetSpellData(slot).name and myHero:CanUseSpell(slot) == READY and (myHero:GetSpellData(slot).name:lower():find("ward") and not myHero:GetSpellData(slot).name:lower():find("vision")) then
 			return slot
 		end
 	end
 	for slot = ITEM_7, ITEM_1, -1 do
-		if myHero:GetSpellData(slot).name and myHero:CanUseSpell(slot) == READY and string.find(string.lower(myHero:GetSpellData(slot).name), "ward") then
+		if myHero:GetSpellData(slot).name and myHero:CanUseSpell(slot) == READY and myHero:GetSpellData(slot).name:lower():find("ward") then
 			return slot
 		end
 	end
