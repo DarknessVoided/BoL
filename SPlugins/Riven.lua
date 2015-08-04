163f5a0433cb81a43b7230e2239c72693bcd9321
nebelwolfi <dr.backdooring@gmail.com>
2015-08-04 09:26:32 +0200

    asdf

--- SPlugins/Riven.lua
+++ SPlugins/Riven.lua
@@ -70,7 +70,7 @@ class "Riven"
     if Config.Misc.Jump then
       self.jumpPos = myHero + (Vector(mousePos) - myHero):normalized() * 50
       self.movePos = myHero + (Vector(mousePos) - myHero):normalized() * 225
-      if self.QCast < 2 then
+      if self.QCast < 3 then
         Cast(_Q, myHero.pos)
       end
       if not IsWall(D3DXVECTOR3(self.jumpPos.x,self.jumpPos.y,self.jumpPos.z)) then
@@ -90,10 +90,13 @@ class "Riven"
 
   function Riven:ProcessSpell(unit,spell)
     if unit and unit.isMe and spell then
+      print(spell.name)
       if spell.name == "RivenTriCleave" then
         self.QCast = self.QCast + 1
         DelayAction(function() if not sReady[_Q] then self.QCast = 0 end end, 4)
-        self:AfterQ()
+        if self.QAA and self.QCast < 3 then 
+          self:AfterQ()
+        end
       elseif spell.name == "RivenFeint" then
         self.EDelay = GetTickCount()
         if self.Target and Config.kConfig.Combo and myHero:CanUseSpell(_R) == READY and Config.Combo.Rm > 1 and (Config.Combo.Rm == 4 or self:CalcComboDmg(self.Target, 0) * (Config.Combo.Rm == 2 and 1.67 or 1) >= self.Target.health) and (Config.Combo.Rm ~= 3 or self:CalcComboDmg(self.Target, 0, true) <= self.Target.health) and myHero:GetSpellData(_R).name == "RivenFengShuiEngine" then 
@@ -112,6 +115,10 @@ class "Riven"
             else
             end
           end, spell.windUpTime - GetLatency() / 2000 + 0.07)
+        end
+      elseif spell.name:lower():find("rivenizunablade") then
+        if self.Target and Config.kConfig.Combo and Config.Combo.Rm > 1 then
+          Cast(_Q, self.Target.pos)
         end
       end
     end
@@ -255,6 +262,8 @@ class "Riven"
     end
     if myHero.isWindingUp and Config.Combo.Rm > 1 and (GetDmg(_R,myHero,self.Target)+GetDmg(_Q,myHero,self.Target)+GetDmg("AD",myHero,self.Target)+self:DmgP(self.Target,myHero.totalDamage*1.2) >= self.Target.health) and myHero:GetSpellData(_R).name ~= "RivenFengShuiEngine" then Cast(_R, self.Target.pos) end
     if Config.Combo.Rm > 1 and GetDmg(_R,myHero,self.Target) >= self.Target.health and myHero:GetSpellData(_R).name ~= "RivenFengShuiEngine" then Cast(_R, self.Target.pos) end
+    if Config.Combo.Rm > 1 and self.Target.health/self.Target.maxHealth <= 0.25 and myHero:GetSpellData(_R).name ~= "RivenFengShuiEngine" then Cast(_R, self.Target.pos) end
+    if not myHero.isWindingUp and Config.Combo.Rm > 1 and self.QCast == 2 and myHero:GetSpellData(_R).name ~= "RivenFengShuiEngine" then Cast(_R, self.Target.pos) end
     if sReady[_W] and GetDistance(self.Target) < data[1].range and Config.Combo.W then
       Cast(_W)
     end
