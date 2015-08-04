2a429c95223e459b1323ea94ffd29e0dd3ca6d85
nebelwolfi <dr.backdooring@gmail.com>
2015-08-05 00:02:25 +0200

    asdf

--- SAwareness.lua
+++ SAwareness.lua
@@ -1,4 +1,4 @@
-_G.SAwarenessVersion = 0.3
+_G.SAwarenessVersion = 0.4
 assert(load(Base64Decode("G0x1YVIAAQQEBAgAGZMNChoKAAAAAAAAAAAAAQIKAAAABgBAAEFAAAAdQAABBkBAAGUAAAAKQACBBkBAAGVAAAAKQICBHwCAAAQAAAAEBgAAAGNsYXNzAAQNAAAAU2NyaXB0U3RhdHVzAAQHAAAAX19pbml0AAQLAAAAU2VuZFVwZGF0ZQACAAAAAgAAAAgAAAACAAotAAAAhkBAAMaAQAAGwUAABwFBAkFBAQAdgQABRsFAAEcBwQKBgQEAXYEAAYbBQACHAUEDwcEBAJ2BAAHGwUAAxwHBAwECAgDdgQABBsJAAAcCQQRBQgIAHYIAARYBAgLdAAABnYAAAAqAAIAKQACFhgBDAMHAAgCdgAABCoCAhQqAw4aGAEQAx8BCAMfAwwHdAIAAnYAAAAqAgIeMQEQAAYEEAJ1AgAGGwEQA5QAAAJ1AAAEfAIAAFAAAAAQFAAAAaHdpZAAEDQAAAEJhc2U2NEVuY29kZQAECQAAAHRvc3RyaW5nAAQDAAAAb3MABAcAAABnZXRlbnYABBUAAABQUk9DRVNTT1JfSURFTlRJRklFUgAECQAAAFVTRVJOQU1FAAQNAAAAQ09NUFVURVJOQU1FAAQQAAAAUFJPQ0VTU09SX0xFVkVMAAQTAAAAUFJPQ0VTU09SX1JFVklTSU9OAAQEAAAAS2V5AAQHAAAAc29ja2V0AAQIAAAAcmVxdWlyZQAECgAAAGdhbWVTdGF0ZQAABAQAAAB0Y3AABAcAAABhc3NlcnQABAsAAABTZW5kVXBkYXRlAAMAAAAAAADwPwQUAAAAQWRkQnVnc3BsYXRDYWxsYmFjawABAAAACAAAAAgAAAAAAAMFAAAABQAAAAwAQACBQAAAHUCAAR8AgAACAAAABAsAAABTZW5kVXBkYXRlAAMAAAAAAAAAQAAAAAABAAAAAQAQAAAAQG9iZnVzY2F0ZWQubHVhAAUAAAAIAAAACAAAAAgAAAAIAAAACAAAAAAAAAABAAAABQAAAHNlbGYAAQAAAAAAEAAAAEBvYmZ1c2NhdGVkLmx1YQAtAAAAAwAAAAMAAAAEAAAABAAAAAQAAAAEAAAABAAAAAQAAAAEAAAABAAAAAUAAAAFAAAABQAAAAUAAAAFAAAABQAAAAUAAAAFAAAABgAAAAYAAAAGAAAABgAAAAUAAAADAAAAAwAAAAYAAAAGAAAABgAAAAYAAAAGAAAABgAAAAYAAAAHAAAABwAAAAcAAAAHAAAABwAAAAcAAAAHAAAABwAAAAcAAAAIAAAACAAAAAgAAAAIAAAAAgAAAAUAAABzZWxmAAAAAAAtAAAAAgAAAGEAAAAAAC0AAAABAAAABQAAAF9FTlYACQAAAA4AAAACAA0XAAAAhwBAAIxAQAEBgQAAQcEAAJ1AAAKHAEAAjABBAQFBAQBHgUEAgcEBAMcBQgABwgEAQAKAAIHCAQDGQkIAx4LCBQHDAgAWAQMCnUCAAYcAQACMAEMBnUAAAR8AgAANAAAABAQAAAB0Y3AABAgAAABjb25uZWN0AAQRAAAAc2NyaXB0c3RhdHVzLm5ldAADAAAAAAAAVEAEBQAAAHNlbmQABAsAAABHRVQgL3N5bmMtAAQEAAAAS2V5AAQCAAAALQAEBQAAAGh3aWQABAcAAABteUhlcm8ABAkAAABjaGFyTmFtZQAEJgAAACBIVFRQLzEuMA0KSG9zdDogc2NyaXB0c3RhdHVzLm5ldA0KDQoABAYAAABjbG9zZQAAAAAAAQAAAAAAEAAAAEBvYmZ1c2NhdGVkLmx1YQAXAAAACgAAAAoAAAAKAAAACgAAAAoAAAALAAAACwAAAAsAAAALAAAADAAAAAwAAAANAAAADQAAAA0AAAAOAAAADgAAAA4AAAAOAAAACwAAAA4AAAAOAAAADgAAAA4AAAACAAAABQAAAHNlbGYAAAAAABcAAAACAAAAYQAAAAAAFwAAAAEAAAAFAAAAX0VOVgABAAAAAQAQAAAAQG9iZnVzY2F0ZWQubHVhAAoAAAABAAAAAQAAAAEAAAACAAAACAAAAAIAAAAJAAAADgAAAAkAAAAOAAAAAAAAAAEAAAAFAAAAX0VOVgA="), nil, "bt", _ENV))() ScriptStatus("REHGLKDMKFG") 
 
 function OnLoad()
@@ -23,15 +23,21 @@ class "SAwareness"
     self.Config:addParam("wpa", "Allies:", SCRIPT_PARAM_ONOFF, false)
     self.Config:addParam("i", "", SCRIPT_PARAM_INFO, "")
     self.Config:addParam("war", "Enemy Wards", SCRIPT_PARAM_ONOFF, true)
+    self.Config:addParam("i", "", SCRIPT_PARAM_INFO, "")
+    self.Config:addParam("ssr", "Enemy Skillshots", SCRIPT_PARAM_ONOFF, true)
     AddDrawCallback(function() self:Draw() end)
     AddUnloadCallback(function() self:Unload() end)
     AddCreateObjCallback(function(obj) self:CreateObj(obj) end)
     AddDeleteObjCallback(function(obj) self:DeleteObj(obj) end)
+    AddProcessSpellCallback(function(unit, spell) self:ProcessSpell(unit, spell) end)
     UpdateWindow()
-    self:Load()
     self:Update()
     self.offset = 18
     self.enemyWards = {}
+    self.activeSpells = {}
+    self.str = {[_Q] = "Q", [_W] = "W", [_E] = "E", [_R] = "R"}
+    self:Load()
+    self:LoadSSR()
     return self
   end
 
@@ -66,6 +72,17 @@ class "SAwareness"
     print("<font color=\"#6699ff\"><b>[Scriptology Awareness]: </b></font> <font color=\"#FFFFFF\">loaded.</font>") 
   end
 
+  function SAwareness:LoadSSR()
+    if FileExist(LIB_PATH .. "SpellData.lua") then
+      self.data = loadfile(LIB_PATH .. "SpellData.lua")()
+      self.loadedSSR = true
+    else
+      DownloadFile("https://raw.github.com/nebelwolfi/BoL/master/Common/SpellData.lua".."?rand="..math.random(1,10000), LIB_PATH.."SpellData.lua", function () end)
+      DelayAction(function() self:LoadSSR() end, 0.5)
+      return
+    end
+  end
+
   function SAwareness:Unload()
     for _,k in pairs(self.Sprites) do
         k:Release()
@@ -89,6 +106,22 @@ class "SAwareness"
       for _, k in pairs(self.enemyWards) do
         if k.object == obj then
           k = nil
+        end
+      end
+    end
+  end
+
+  function SAwareness:ProcessSpell(unit, spell)
+    if self.Config.ssr and self.loadedSSR and unit and spell and spell.name and unit.team ~= myHero.team then
+      if spell.name:lower():find("attack") or spell.target then return end
+      if self.data and self.data[unit.charName] then
+        for i=0,3 do
+          if (not self.data[unit.charName][i] or self.data[unit.charName][i].name == "" or not self.data[unit.charName][i].name) then
+            --print("Unsupported spell found!! "..unit.charName.." "..spell.name)
+          elseif spell.name:find(self.data[unit.charName][i].name) then
+            s = {slot = i, source = unit, startTime = GetInGameTimer(), startPos = Vector(unit), endPos = Vector(spell.endPos), windUpTime = spell.windUpTime, name = spell.name}
+            table.insert(self.activeSpells, s)
+          end
         end
       end
     end
@@ -123,6 +156,7 @@ class "SAwareness"
     self:DrawWPA()
     self:DrawSCD()
     self:DrawWAR()
+    self:DrawSSR()
   end
 
   function SAwareness:DrawCDE()
@@ -186,6 +220,11 @@ class "SAwareness"
           local IndexPath = k:GetPath(k.pathIndex)
           if IndexPath then
             DrawCircle3D(IndexPath.x, IndexPath.y, IndexPath.z, 18, 1, ARGB(105,255,255,255), 32)
+            local barPos = WorldToScreen(D3DXVECTOR3(IndexPath.x, IndexPath.y, IndexPath.z))
+            local posX = barPos.x
+            local posY = barPos.y
+            DrawText(""..k.charName, 15, posX, posY-10, ARGB(155, 255, 255, 255))
+            DrawText((math.floor(GetDistance(k,IndexPath)/k.ms*10)/10).."s", 15, posX, posY+10, ARGB(155, 255, 255, 255))
             DrawLine3D(k.x, k.y, k.z, IndexPath.x, IndexPath.y, IndexPath.z, 1, ARGB(105, 255, 255, 255))
           end
           for i=k.pathIndex, k.pathCount-1 do
@@ -205,6 +244,11 @@ class "SAwareness"
           local IndexPath = k:GetPath(k.pathIndex)
           if IndexPath then
             DrawCircle3D(IndexPath.x, IndexPath.y, IndexPath.z, 18, 1, ARGB(105,255,255,255), 32)
+            local barPos = WorldToScreen(D3DXVECTOR3(IndexPath.x, IndexPath.y, IndexPath.z))
+            local posX = barPos.x
+            local posY = barPos.y
+            DrawText(""..k.charName, 15, posX, posY-10, ARGB(155, 255, 255, 255))
+            DrawText((math.floor(GetDistance(k,IndexPath)/k.ms*10)/10).."s", 15, posX, posY+10, ARGB(155, 255, 255, 255))
             DrawLine3D(k.x, k.y, k.z, IndexPath.x, IndexPath.y, IndexPath.z, 1, ARGB(105, 255, 255, 255))
           end
           for i=k.pathIndex, k.pathCount-1 do
@@ -272,5 +316,87 @@ class "SAwareness"
           DrawText("Vision Ward", 20, posX, posY, ARGB(255, 255, 0, 0))
         end
       end
+    end
+  end
+
+  function SAwareness:DrawSSR()
+    if self.Config.ssr then
+      for _,spell in pairs(self.activeSpells) do
+        local range = self.data[spell.source.charName][spell.slot].range
+        local width = self.data[spell.source.charName][spell.slot].width
+        local speed = self.data[spell.source.charName][spell.slot].speed
+        local delay = self.data[spell.source.charName][spell.slot].delay
+        local type  = self.data[spell.source.charName][spell.slot].type
+        if speed ~= math.huge then
+          if type == "linear" then
+            if spell.startTime+range/speed+delay+self:GetGroundTime(spell.source, spell.slot) > GetInGameTimer() then
+              if GetDistanceSqr(spell.startPos,spell.endPos) < range * range then
+                spell.endPos = spell.startPos+Vector(Vector(spell.endPos)-spell.startPos):normalized()*range
+              end
+              local pos = spell.startPos+Vector(Vector(spell.endPos)-spell.startPos):normalized()*(speed*(GetInGameTimer()-delay-spell.startTime)-width)
+              self:DrawRectangleOutline(spell.startPos, spell.endPos, (spell.startTime+delay < GetInGameTimer() and pos or nil), width)
+            else
+              table.remove(self.activeSpells, _)
+            end
+          end
+          if type == "circular" then
+            if spell.startTime+range/speed+delay+self:GetGroundTime(spell.source, spell.slot) > GetInGameTimer() then
+              DrawCircle3D(spell.endPos.x, spell.endPos.y, spell.endPos.z, width, 2, ARGB(255, 255, 255, 255), 32)
+              DrawCircle3D(spell.endPos.x, spell.endPos.y, spell.endPos.z, width-10, 2, ARGB(255, 255, 255, 255), 32)
+            else
+              table.remove(self.activeSpells, _)
+            end
+          end
+        elseif speed == math.huge then
+          if type == "circular" then
+            if spell.startTime+delay+self:GetGroundTime(spell.source, spell.slot) > GetInGameTimer() then
+              DrawCircle3D(spell.endPos.x, spell.endPos.y, spell.endPos.z, width, 2, ARGB(255, 255, 255, 255), 32)
+              DrawCircle3D(spell.endPos.x, spell.endPos.y, spell.endPos.z, width-10, 2, ARGB(255, 255, 255, 255), 32)
+            else
+              table.remove(self.activeSpells, _)
+            end
+          end
+          if type == "linear" then
+            if spell.startTime+delay+self:GetGroundTime(spell.source, spell.slot) > GetInGameTimer() then
+              if GetDistanceSqr(spell.startPos,spell.endPos) < range * range then
+                spell.endPos = spell.startPos+Vector(Vector(spell.endPos)-spell.startPos):normalized()*range
+              end
+              self:DrawRectangleOutline(spell.startPos, spell.endPos, nil, width)
+            else
+              table.remove(self.activeSpells, _)
+            end
+          end
+        end
+      end
+    end
+  end
+
+  function SAwareness:GetGroundTime(unit, spell)
+    if unit.charName == "Lux" and spell == 2 then return 1 end
+    if unit.charName == "Lux" and spell == 3 then return 0.25 end
+    if unit.charName == "Ziggs" and spell == 1 then return 1 end
+    if unit.charName == "Ziggs" and spell == 2 then return 1 end
+    return 0
+  end
+
+  function SAwareness:DrawRectangleOutline(startPos, endPos, pos, width)
+    local c1 = startPos+Vector(Vector(endPos)-startPos):perpendicular():normalized()*width/2
+    local c2 = startPos+Vector(Vector(endPos)-startPos):perpendicular2():normalized()*width/2
+    local c3 = endPos+Vector(Vector(startPos)-endPos):perpendicular():normalized()*width/2
+    local c4 = endPos+Vector(Vector(startPos)-endPos):perpendicular2():normalized()*width/2
+    DrawLine3D(c1.x,c1.y,c1.z,c2.x,c2.y,c2.z,math.ceil(width/100),ARGB(55, 255, 255, 255))
+    DrawLine3D(c2.x,c2.y,c2.z,c3.x,c3.y,c3.z,math.ceil(width/100),ARGB(55, 255, 255, 255))
+    DrawLine3D(c3.x,c3.y,c3.z,c4.x,c4.y,c4.z,math.ceil(width/100),ARGB(55, 255, 255, 255))
+    DrawLine3D(c1.x,c1.y,c1.z,c4.x,c4.y,c4.z,math.ceil(width/100),ARGB(55, 255, 255, 255))
+    local c1 = startPos+Vector(Vector(endPos)-startPos):perpendicular():normalized()*width
+    local c2 = startPos+Vector(Vector(endPos)-startPos):perpendicular2():normalized()*width
+    local c3 = endPos+Vector(Vector(startPos)-endPos):perpendicular():normalized()*width
+    local c4 = endPos+Vector(Vector(startPos)-endPos):perpendicular2():normalized()*width
+    DrawLine3D(c1.x,c1.y,c1.z,c2.x,c2.y,c2.z,math.ceil(width/100),ARGB(25, 255, 255, 255))
+    DrawLine3D(c2.x,c2.y,c2.z,c3.x,c3.y,c3.z,math.ceil(width/100),ARGB(25, 255, 255, 255))
+    DrawLine3D(c3.x,c3.y,c3.z,c4.x,c4.y,c4.z,math.ceil(width/100),ARGB(25, 255, 255, 255))
+    DrawLine3D(c1.x,c1.y,c1.z,c4.x,c4.y,c4.z,math.ceil(width/100),ARGB(25, 255, 255, 255))
+    if pos then
+      DrawCircle3D(pos.x, pos.y, pos.z, width/2, 1, ARGB(155, 255, 255, 255), 8)
     end
   end
