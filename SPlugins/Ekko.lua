ddd7e8cfb3d35cdc2ebbcf2fa0442984ba367022
nebelwolfi <dr.backdooring@gmail.com>
2015-08-05 22:37:39 +0200

    asdf

--- SPlugins/Ekko.lua
+++ SPlugins/Ekko.lua
@@ -8,10 +8,36 @@ class "Ekko"
       [_E] = { delay = 0.50, range = 350, dmgAP = function(AP, level, Level, TotalDmg, source, target) return 30*level+20+0.2*AP+TotalDmg end},
       [_R] = { speed = math.huge, delay = 0.5, range = 0, width = 400, collision = false, aoe = true, type = "circular", dmgAP = function(AP, level, Level, TotalDmg, source, target) return 150*level+50+1.3*AP end}
     }
+    objTrackList = { 
+      "Ekko", 
+      "Ekko_Base_Q_Aoe_Dilation.troy", 
+      "Ekko_Base_W_Detonate_Slow.troy", 
+      "Ekko_Base_W_Indicator.troy", 
+      "Ekko_Base_W_Cas.troy"
+    }
+    objTimeTrackList = { 
+      math.huge,
+      1.565, 
+      1.70, 
+      3, 
+      1
+    }
+    objHolder = {}
+    objTimeHolder = {}
   end
 
   function Ekko:Load()
     SetupMenu()
+    table.insert(objHolder, myHero)
+    for k=1,objManager.maxObjects,1 do
+      local object = objManager:getObject(k)
+      for _,name in pairs(objTrackList) do
+        if object and object.valid and object.name and object.team == myHero.team and object.name == name then
+          objHolder[name] = object
+          if ScriptologyDebug then print("Object "..object.name.." already created. Now tracking!") end
+        end
+      end
+    end
   end
 
   function Ekko:Menu()
@@ -41,6 +67,69 @@ class "Ekko"
     Config.kConfig:addDynamicParam("LaneClear", "Lane Clear", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("V"))
   end
 
+  function Ekko:CreateObj(object)
+    if object and object.valid and object.name then
+      for _,name in pairs(objTrackList) do
+        if object.name == name then
+          objHolder[object.networkID] = object
+          objTimeHolder[object.networkID] = GetInGameTimer() + objTimeTrackList[_]
+          if ScriptologyDebug then print("Object "..object.name.." created. Now tracking!") end
+        end
+      end
+    end
+  end
+   
+  function Ekko:DeleteObj(object)
+    if object and object.name then 
+      for _,name in pairs(objTrackList) do
+        if object.name == name then
+          objHolder[object.networkID] = nil
+          if ScriptologyDebug then print("Object "..object.name.." destroyed. No longer tracking! "..#objHolder) end
+        end
+      end
+    end
+  end
+
+  function Ekko:Draw()
+    if #objHolder > 0 then
+      for _,obj in pairs(objHolder) do
+        if obj ~= myHero then
+          if objTimeHolder[obj.networkID] and objTimeHolder[obj.networkID] < math.huge then
+            if objTimeHolder[obj.networkID]>GetInGameTimer() then 
+              local barPos = WorldToScreen(D3DXVECTOR3(obj.x, obj.y, obj.z))
+              local posX = barPos.x - 35
+              local posY = barPos.y - 50
+              if myHero.charName ~= "Azir" or Config.Draws.W then DrawText((math.floor((objTimeHolder[obj.networkID]-GetInGameTimer())*100)/100).."s", 25, posX, posY, ARGB(255, 255, 0, 0)) end
+            else
+              objHolder[obj.networkID] = nil
+              objTimeHolder[obj.networkID] = nil
+            end
+          end
+          width = myHero.charName == "Ekko" and obj.name == "Ekko" and data[3].width or (((myHero.charName == "Ekko" and obj.name:find("Ekko_Base_W")) or myHero.charName == "Azir") and data[1].width or data[0].width)
+          if myHero.charName == "Ekko" then
+            if obj.name == "Ekko" and Config.Draws.R then 
+              DrawLFT(obj.x, obj.y, obj.z, width, ARGB(155, 155, 150, 250)) 
+              DrawLFC(obj.x, obj.y, obj.z, width, ARGB(255, 155, 150, 250))
+            elseif obj.name:find("Ekko_Base_Q") and Config.Draws.Q then 
+              DrawLine3D(myHero.x, myHero.y, myHero.z, obj.x, obj.y, obj.z, 1, ARGB(255, 155, 150, 250)) 
+              DrawLFC(obj.x, obj.y, obj.z, width, ARGB(255, 155, 150, 250))
+            elseif Config.Draws.W then
+              DrawLFC(obj.x, obj.y, obj.z, width, ARGB(255, 155, 150, 250))
+            end
+          elseif myHero.charName == "Orianna" then
+            DrawCircle(obj.x-8, obj.y, obj.z+87, data[0].width-50, 0x111111)
+            for i=0,2 do
+              LagFree(obj.x-8, obj.y, obj.z+87, data[0].width-50, 3, ARGB(255, 75, 0, 230), 3, (math.pi/4.5)*(i))
+            end 
+            LagFree(obj.x-8, obj.y, obj.z+87, data[0].width-50, 3, ARGB(255, 75, 0, 230), 9, 0)
+          elseif myHero.charName ~= "Azir" or Config.Draws.W then
+            DrawLFC(obj.x, obj.y, obj.z, width, ARGB(255, 155, 150, 250))
+          end
+        end
+      end
+    end
+  end
+
   function Ekko:GetTwin()
     local twin = nil
     for _,k in pairs(objHolder) do
@@ -64,7 +153,7 @@ class "Ekko"
       for minion,winion in pairs(Mobs.objects) do
         local MinionDmg = GetDmg(_E, myHero, winion)
         if MinionDmg and MinionDmg >= winion.health and ValidTarget(winion, data[2].range+myHero.range+myHero.boundingRadius) and GetDistance(winion) < data[2].range+myHero.range+myHero.boundingRadius then
-          Cast(_E, winion)
+          Cast(_E, winion.pos)
         end
       end
     end
@@ -88,13 +177,13 @@ class "Ekko"
     end
     if GetLichSlot() then
       if myHero:GetSpellData(GetLichSlot()).currentCd == 0 and Config.Combo.E and ValidTarget(Target, data[2].range+(myHero.range+myHero.boundingRadius*2)*2) then
-        Cast(_E, Target)
+        Cast(_E, Target.pos)
       elseif myHero:GetSpellData(GetLichSlot()).currentCd > 0 and Config.Combo.E and ValidTarget(Target, data[2].range+(myHero.range+myHero.boundingRadius*2)*2) and GetDistance(Target) > myHero.range+myHero.boundingRadius*2 then
-        Cast(_E, Target)
+        Cast(_E, Target.pos)
       end
     else
       if Config.Combo.E and ValidTarget(Target, data[2].range+(myHero.range+myHero.boundingRadius)*2) then
-        Cast(_E, Target)
+        Cast(_E, Target.pos)
       end
     end
   end
@@ -105,13 +194,13 @@ class "Ekko"
     end
     if GetLichSlot() then
       if myHero:GetSpellData(GetLichSlot()).currentCd == 0 and Config.Harrass.manaE < myHero.mana/myHero.maxMana*100 and Config.Harrass.E and ValidTarget(Target, data[2].range+(myHero.range+myHero.boundingRadius*2)*2) then
-        Cast(_E, Target)
+        Cast(_E, Target.pos)
       elseif myHero:GetSpellData(GetLichSlot()).currentCd > 0 and Config.Harrass.manaE < myHero.mana/myHero.maxMana*100 and Config.Harrass.E and ValidTarget(Target, data[2].range+(myHero.range+myHero.boundingRadius*2)*2) and GetDistance(Target) > myHero.range+myHero.boundingRadius*2 then
-        Cast(_E, Target)
+        Cast(_E, Target.pos)
       end
     else
       if Config.Harrass.E and Config.Harrass.manaE < myHero.mana/myHero.maxMana*100 and ValidTarget(Target, data[2].range+(myHero.range+myHero.boundingRadius)*2) then
-        Cast(_E, Target)
+        Cast(_E, Target.pos)
       end
     end
   end
@@ -122,9 +211,9 @@ class "Ekko"
         if myHero:CanUseSpell(_Q) == READY and GetRealHealth(enemy) < GetDmg(_Q, myHero, enemy) and Config.Killsteal.Q and ValidTarget(enemy, data[0].range) then
           Cast(_Q, enemy, 1.2)
         elseif myHero:CanUseSpell(_E) == READY and GetRealHealth(enemy) < GetDmg(_E, myHero, enemy) and Config.Killsteal.E and ValidTarget(enemy, data[2].range+(myHero.range+myHero.boundingRadius)*2) then
-          Cast(_E, enemy)
+          Cast(_E, enemy.pos)
         elseif myHero:CanUseSpell(_R) == READY and GetRealHealth(enemy) < GetDmg(_R, myHero, enemy) and Config.Killsteal.R and ValidTarget(enemy, data[3].range) then
-          Cast(_R, enemy, self:GetTwin())
+          Cast(_R, enemy, 1, self:GetTwin())
         elseif Ignite and myHero:CanUseSpell(Ignite) == READY and GetRealHealth(enemy) < (50 + 20 * myHero.level) and Config.Killsteal.I and ValidTarget(enemy, 600) then
           CastSpell(Ignite, enemy)
         end
