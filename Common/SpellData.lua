590f01cfe19b15ebb9efece3d8989487b1d510b0
nebelwolfi <dr.backdooring@gmail.com>
2015-08-22 09:26:16 +0200

    asdf

--- Common/SpellData.lua
+++ Common/SpellData.lua
@@ -76,7 +76,7 @@ return {
 		[_Q] = { name = "", speed = math.huge, delay = 0.75, range = 450, width = 450, type = "circular", dmgAD = function(source, target) return 20*source:GetSpellData(_Q).level+(0.9 + 0.1 * source:GetSpellData(_Q).level)*source.totalDamage end},
 		[_W] = { name = "", range = myHero.range+myHero.boundingRadius*2, dmgAD = function(source, target) return source.totalDamage*1.4 end},
 		[_E] = { name = "", speed = 1500, delay = 0.25, range = 575, width = 0.25, collision = false, aoe = true, type = "cone"},
-		[_R] = { name = "", range = 475, dmgTRUE = function(source, target, stacks) return math.floor(99*source:GetSpellData(_R).level+0.749*source.addDamage+stacks*(19*source:GetSpellData(_R).level+0.149*source.addDamage)) end}
+		[_R] = { name = "", range = 460, dmgTRUE = function(source, target, stacks) return math.floor(99*source:GetSpellData(_R).level+0.749*source.addDamage+stacks*(19*source:GetSpellData(_R).level+0.149*source.addDamage)) end}
 	},
 	["Diana"] = {
 		[_Q] = { name = "DianaArc", speed = 1500, delay = 0.250, range = 835, width = 130, collision = false, aoe = false, type = "circular", dmgAP = function(source, target) return 35*source:GetSpellData(_Q).level+45+0.2*source.ap end},
