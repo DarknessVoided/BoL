be8467b33d7c9b4e390d3dc7506f0d7406adcefc
nebelwolfi <dr.backdooring@gmail.com>
2015-08-27 04:17:43 +0200

    asdf

--- Scriptology.lua
+++ Scriptology.lua
@@ -4919,7 +4919,10 @@ class "Yorick"
         else
           for _=0, 3 do
             if myHero:CanUseSpell(_) == READY and Config.Combo[str[_]] and GetDistanceSqr(enemy) < myHeroSpellData[_].range^2 then
+              local tempPred = Config.Prediction.Combo.Q
+              Config.Prediction.Combo.Q = 0
               Cast(_, enemy)
+              Config.Prediction.Combo.Q = tempPred
               break;
             end
           end
