dbb13db69e024c7a115f6e10069eba61df4c3e31
nebelwolfi <dr.backdooring@gmail.com>
2015-09-01 12:27:26 +0200

    asdf

--- Scriptology.lua
+++ Scriptology.lua
@@ -10,7 +10,7 @@ local min, max, cos, sin, pi, huge, ceil, floor, round, random, abs, deg, asin, 
 -- { Load
 
   function OnLoad()
-    Update()
+    --Update()
     LoadSpellData()
     LoadActivator()
     LoadAwareness()
@@ -22,17 +22,19 @@ local min, max, cos, sin, pi, huge, ceil, floor, round, random, abs, deg, asin, 
   end
 
   function Update()
-    local ToUpdate = {}
-    ToUpdate.UseHttps = true
-    ToUpdate.Host = "raw.githubusercontent.com"
-    ToUpdate.VersionPath = "/nebelwolfi/BoL/master/Scriptology.version"
-    ToUpdate.ScriptPath =  "/nebelwolfi/BoL/master/Scriptology.lua"
-    ToUpdate.SavePath = SCRIPT_PATH..GetCurrentEnv().FILE_NAME
-    ToUpdate.CallbackUpdate = function(NewVersion,OldVersion) Msg("Updated from v"..OldVersion.." to "..NewVersion..". Please press F9 twice to reload.") end
-    ToUpdate.CallbackNoUpdate = function(OldVersion) end
-    ToUpdate.CallbackNewVersion = function(NewVersion) Msg("New version found v"..NewVersion..". Please wait until it's downloaded.") end
-    ToUpdate.CallbackError = function(NewVersion) Msg("There was an error while updating.") end
-    CScriptUpdate(_G.ScriptologyVersion,ToUpdate.UseHttps, ToUpdate.Host, ToUpdate.VersionPath, ToUpdate.ScriptPath, ToUpdate.SavePath, ToUpdate.CallbackUpdate,ToUpdate.CallbackNoUpdate, ToUpdate.CallbackNewVersion,ToUpdate.CallbackError)
+    if _G.ScriptologyAutoUpdate then
+      local ToUpdate = {}
+      ToUpdate.UseHttps = true
+      ToUpdate.Host = "raw.githubusercontent.com"
+      ToUpdate.VersionPath = "/nebelwolfi/BoL/master/Scriptology.version"
+      ToUpdate.ScriptPath =  "/nebelwolfi/BoL/master/Scriptology.lua"
+      ToUpdate.SavePath = SCRIPT_PATH..GetCurrentEnv().FILE_NAME
+      ToUpdate.CallbackUpdate = function(NewVersion,OldVersion) Msg("Updated from v"..OldVersion.." to "..NewVersion..". Please press F9 twice to reload.") end
+      ToUpdate.CallbackNoUpdate = function(OldVersion) end
+      ToUpdate.CallbackNewVersion = function(NewVersion) Msg("New version found v"..NewVersion..". Please wait until it's downloaded.") end
+      ToUpdate.CallbackError = function(NewVersion) Msg("There was an error while updating.") end
+      CScriptUpdate(_G.ScriptologyVersion,ToUpdate.UseHttps, ToUpdate.Host, ToUpdate.VersionPath, ToUpdate.ScriptPath, ToUpdate.SavePath, ToUpdate.CallbackUpdate,ToUpdate.CallbackNoUpdate, ToUpdate.CallbackNewVersion,ToUpdate.CallbackError)
+    end
   end
 
   function LoadSpellData()
